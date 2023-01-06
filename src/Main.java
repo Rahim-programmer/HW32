f0344348a9b291ad79f1ceb1f21719e773a7ef44
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-01-06 15:46:57 +0600

    do 1,2 and 3 tasks

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
-import org.w3c.dom.events.Event;
-
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 import java.util.stream.Collectors.*;
+
+import static java.util.stream.Collectors.toMap;
 
 public class Main {
 
@@ -11,13 +12,61 @@ public class Main {
 
     static Scanner sc = new Scanner(System.in);
 
-    static List<String, Event> eventMap = new HashMap<>();
+    static Map<String, Event> eventMap = new HashMap<>();
 
     static {
-        eventMap.add("F", Event.ADD_COIN)
+        eventMap.put("a", Event.ADD_COIN);
+        eventMap.put("q", Event.QUIT);
     }
-    public static void main(String[]args){
-    while (machine.isWork()){
-        if(machine.g)
+
+    public static void main(String[] args) {
+        while (machine.isWork()) {
+            if(machine.getProductList().size() > 0){
+                System.out.println("В автомате доступны:");
+                machine.getProductList().forEach((k, v) -> {
+                    System.out.printf("[%d] - %s\n", v.getPrice(), v.getName());
+                });
+                System.out.println("Монет на сумму - " + machine.getMoney());
+                events();
+            }else {
+                System.out.println("В автомате отсутствуют товары, приходите завтра!");
+                machine.closeMachine();
+            }
+        }
+
+    }
+    public static void events(){
+        String action;
+        System.out.println("Что хотите сделать:");
+        System.out.printf("a - %s\n", eventMap.get("a").getValue());
+
+        machine.getProductList().entrySet()
+                .stream()
+                .filter(e -> e.getValue().getPrice() <= machine.getMoney())
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .forEach((k, v) -> {
+                    eventMap.put(k, Event.BUY);
+                });
+        eventMap.entrySet().removeIf(entry -> machine.getProductList().get(entry.getKey()) != null && machine.getProductList().get(entry.getKey()).getPrice() > machine.getMoney());
+        eventMap.entrySet()
+                .stream()
+                .filter(e -> !e.getKey().equalsIgnoreCase("a") & !e.getKey().equalsIgnoreCase("q"))
+                .collect(toMap(Map.Entry::getKey, Map.Entry::getValue))
+                .forEach((k, v) -> {
+                    System.out.printf("%s - купить '%s'\n", k, machine.getProductList().get(k).getName());
+                });
+
+        System.out.printf("q - %s\n", eventMap.get("q").getValue());
+        System.out.print(">? ");
+        action = sc.nextLine();
+        if(eventMap.get(action) != null) {
+            eventMap.get(action).run(machine, action);
+            if(!action.equalsIgnoreCase("a") & !action.equalsIgnoreCase("q")){
+                eventMap.remove(action);
+            }
+        }else {
+            System.out.println("Такого действия/товара нету!");
+        }
+
     }
 }
