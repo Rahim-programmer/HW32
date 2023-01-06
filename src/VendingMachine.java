624f2013b544edc99d40dd35906b4cd241719356
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-01-06 15:46:42 +0600

    add methods in VendingMachine

--- src/VendingMachine.java
+++ src/VendingMachine.java
@@ -1,8 +1,12 @@
+import java.util.Map;
 import java.util.stream.Collectors;
+
+import static java.util.stream.Collectors.toMap;
 
 public class VendingMachine {
     private int money;
     private boolean work;
+    private final Map<String, Product> productList;
 
 
     public boolean isWork() {
@@ -12,13 +16,32 @@ public class VendingMachine {
     public void setWork(boolean work) {
         this.work = work;
     }
-    public static void closeMachine(){
+    public  void closeMachine(){
         setWork(false);
     }
-    public VendingMachine(){
-        product = Product.makeProduct(5).stream()
-                .collect(toMap(Product::getId, product - > product));
+    public VendingMachine() {
+        productList = Product.makeProduct(5).stream()
+                .collect(toMap(Product::getId, product -> product));
         work = true;
+    }
+
+    public void addMoney(int money){
+        setMoney(getMoney() + money);
+    }
+    public void deleteMoney(int money){
+        setMoney(Math.max(getMoney() - money, 0));
+    }
+
+    public Map<String, Product> getProductList() {
+        return productList;
+    }
+
+    public int getMoney() {
+        return money;
+    }
+
+    public void setMoney(int money) {
+        this.money = money;
     }
 }
 
